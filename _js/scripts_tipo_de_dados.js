e9d72eb337d0dfebc710f9b65ddf6d0b557f57fe
victor-ivanps <vpilotosantos@gmail.com>
2023-10-23 16:02:30 -0300

    Estudando tipos de dados e estrutura de programação no JavaScript

---
+++ _js/scripts_tipo_de_dados.js
@@ -0,0 +1,42 @@
+console.log("Numeros");
+console.log(5);
+console.log(12 + 4 * 5);
+console.log(12 - 4 * 5);
+console.log(typeof 12.7);
+console.log(typeof 5);
+console.log(typeof -5);
+
+
+console.log("String")
+console.log('Isso é uma String')
+console.log("Isso também é uma String")
+console.log(`Loucura, isso também`)
+console.log("Victor Ivan \n Piloto Santos")
+console.log("Isso é uma 'citação'")
+console.log(`A soma de 2 + 2 é ${2+2}`)
+
+console.log('Operadores Logicos')
+console.log(5 > 3 && 3=='3')
+console.log(5 > 3 || 3==1)
+console.log("Felipe" == "João" || false)
+console.log(!true)
+console.log(!false)
+
+console.log('Operador Ternário')
+console.log(true ? 1 : 2)
+console.log(false ? 1 : 2)
+console.log(`20 é par ou impar ? \n ${20 % 2 == 0 ? "Par" : "Impar"}`)
+
+console.log("Varivaeis nulas")
+console.log(undefined)
+console.log(null)
+console.log(typeof undefined)
+console.log(typeof null)
+
+console.log("Conversão automatica de valores");
+console.log('123' + 4);
+console.log('30' - 20);
+console.log('ausd' * 3);
+console.log(null * 3);
+console.log(undefined * 5);
+
