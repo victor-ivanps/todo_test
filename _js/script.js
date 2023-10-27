c47aa96b54572dba71a436d23622710733d8ebfd
victor-ivanps <vpilotosantos@gmail.com>
2023-10-26 23:01:07 -0300

    Avanço no formulario e no entendimento de metodos e classes

--- _js/script.js
+++ _js/script.js
@@ -1,13 +1,106 @@
-function recuperando_elementos(){
-    let formulario = document.getElementsByTagName('form')
+class Tarefa{
+    constructor(nome, descricao, data_inicio, data_fim){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.data_inicio = data_inicio;
+        this.data_fim = data_fim;
+    }
 
-    formulario[0].addEventListener('submit', function(event){
-        event.preventDefault();
-    });
+    get getName(){
+        return this.nome;
+    };
 
-    let titulo = document.getElementsByName('titulo')[0].value;
-    let descr = document.getElementsByName('descricao')[0].value;
+    get getDescricao(){
+        return this.descricao;
+    }
 
-    console.log('Titulo da Atividade:', titulo);
-    console.log('Descrição da Atividade:', descr);
-}
+    get getData_inicio(){
+        return this.data_inicio;
+    };
+
+    get getData_fim(){
+        return this.data_fim;
+    }
+
+    set setName(name){
+        this.name = name;
+    };
+
+    set setDescricao(descricao){
+        this.descricao = descricao;
+    }
+
+    set setData_inicio(data_inicio){
+        this.data_inicio = data_inicio;
+    };
+
+    set setData_fim(data_fim){
+        this.data_fim = data_fim;
+    }
+};
+
+class Lista_Tarefa{
+    constructor(nome){
+        this.nome = nome;
+        this.array_tarefa = [];
+    }
+
+    get getName(){
+        return this.nome;
+    }
+
+    inclui_tarefa(Tarefa){
+        this.array_tarefa.push(Tarefa);
+    }
+
+    show_tarefas(){
+        if(this.array_tarefa.length != 0){
+            const tarefaList = document.getElementsByName('tarefaList')[0];
+            for( let i=0; i < this.array_tarefa.length; i++ ){
+                // console.log('Tarefa ',i+1);
+                // console.log(`${this.array_tarefa[i].getName}`);
+                // console.log(`${this.array_tarefa[i].getDescricao}`);
+                // console.log(`${this.array_tarefa[i].getData_inicio}`);
+                // console.log(`${this.array_tarefa[i].getData_fim}`);
+                // console.log('');
+                let item = document.createElement('li');
+                item.textContent = `Tarefa ${i+1} 
+                \n${this.array_tarefa[i].getName}
+                \n${this.array_tarefa[i].getDescricao} 
+                \n${this.array_tarefa[i].getData_inicio}
+                \n${this.array_tarefa[i].getData_fim}`;
+                tarefaList.appendChild(item);
+            }
+        }
+        else{
+            console.log('Sem Tarefas para começar!!');
+        }
+    }
+
+};
+
+
+let formulario = document.getElementsByTagName('form');
+
+formulario[0].addEventListener('submit', function(event){
+    event.preventDefault();
+
+let titulo = document.getElementsByName('titulo')[0].value;
+let descr = document.getElementsByName('descricao')[0].value;
+let data_inicio = document.getElementsByName('data_inicio')[0].value;
+let data_fim = document.getElementsByName('data_fim')[0].value;
+
+let lista_tarefa = new Lista_Tarefa('Lista 1');
+let tarefa = new Tarefa(titulo, descr, data_inicio, data_fim);
+
+lista_tarefa.inclui_tarefa(tarefa);
+
+lista_tarefa.show_tarefas();
+
+document.getElementsByName('titulo')[0].value = '';
+document.getElementsByName('descricao')[0].value = '';
+document.getElementsByName('data_inicio')[0].value = '';
+document.getElementsByName('data_fim')[0].value = '';
+});
+// recuperando_elementos(Lista_Tarefa);
+// mostrar_tarefas(Lista_Tarefa)
--- _js/script_orientado_objs.js
+++ _js/script_orientado_objs.js
@@ -1,12 +1,124 @@
-let pessoa = {
-    nome:'',
-    setNome : function(novoNome){
-        this.nome = novoNome;
-    },
-    getNome : function(){
-        return this.nome;
-    }
-}
+// let pessoa = {
+//     nome:'',
+//     setNome : function(novoNome){
+//         this.nome = novoNome;
+//     },
+//     getNome : function(){
+//         return this.nome;
+//     },
+//     maos : 2,
+//     pes : 2
+// }
 
-console.log(pessoa.setNome('Victor Ivan'))
-console.log(pessoa.getNome())
+// pessoa.setNome('Victor Ivan');
+
+// let pessoa_nova = Object.create(pessoa); // Usando de Prototypes - Importando já propriedades de pessoas
+
+// pessoa_nova.cabeca = 1;
+// pessoa_nova.setNome('Guilherme');
+
+// console.log(pessoa);
+// console.log(pessoa_nova);
+// console.log(pessoa_nova.hasOwnProperty('pes')); // Dá falso, por que `pes` é uma propriedade de pessoa, mas não de pessoa_nova.
+// console.log(pessoa_nova.maos);
+// console.log(pessoa_nova.pes);
+
+
+// function criaPessoa(nome, idade, peso, tamanho){
+//     let pessoa = Object.create({});
+//     pessoa.nome = nome;
+//     pessoa.idade = idade;
+//     pessoa.peso = peso;
+//     pessoa.tamanho = tamanho;
+
+//     return pessoa;
+// }
+
+// newPeople = criaPessoa('Victor Ivan', 24, 80, 1.75);
+
+// console.log(newPeople);
+
+
+// function Cachorro(nome, raça, cor){
+//     this.nome = nome;
+//     this.raça = raça;
+//     this.cor = cor;
+// }
+
+// let dog = new Cachorro('Cedrik', 'Vira-Lata', 'Cinza-Preto');
+// console.log(dog)
+
+
+
+// function Cachorro(nome, raça, cor){
+//     this.nome = nome;
+//     this.raça = raça;
+//     this.cor = cor;
+// }
+
+// let dog = new Cachorro('Cedrik', 'Vira-Lata', 'Cinza-Preto');
+// // Metodo mais correto de anexar metodos (funções) a uma classe;
+// Cachorro.prototype.uivar = function(){
+//     console.log('Auuuu!!!')
+// };
+
+// console.log(dog)
+// dog.uivar();
+
+
+// Construtor na classe ES6 - New!! 
+
+// class Cachorro1{
+//     constructor(nome, raça, cor){
+//         this.nome = nome;
+//         this.raça = raça;
+//         this.cor = cor;
+//     }
+
+//     get getRaca(){
+//         return this.raça;
+//     }
+
+//     set setRaca(raça){
+//         this.raça = raça;
+//     }
+// }
+
+// let patas = Symbol()
+
+// Cachorro1.prototype[patas] = 4;
+
+// let dog1 = new Cachorro1('Huck', 'Vira-Lata', 'Marron Claro')
+// console.log(dog1);
+
+// Cachorro1.prototype.uivar = function (){
+//     console.log('Auuuuuu!!');
+// }
+
+// dog1.uivar();
+
+// console.log(Cachorro1.prototype[patas]);
+
+// dog1.setRaca = 'Dobberman';
+
+// let raca = dog1.getRaca;
+
+// console.log(raca);
+
+// class Mamifero{
+//     constructor(patas){
+//         this.patas = patas;
+//     }
+// }
+
+// class Cachorro extends Mamifero{
+//     constructor(patas, raça){
+//         super(patas, patas);
+//         this.raça = raça;
+//     }
+// }
+
+// let dog = new Cachorro(4,'Vira-Lata');
+
+// console.log(dog);
+// console.log(new Cachorro instanceof Mamifero); // Verifica se a classe Cachorro extends de Mamifero
